bff2c097ae268fd258cfb1c3cd771eb334d2b4d2
Andrew <echowhite93@yahoo.com>
2022-05-13 10:04:20 -0700

    added more variables and confirmation messages, added multiple criteria selections

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// variables created for password generation input
+// variables created for password generation input and prompt messages
 var message;
+var messageTwo;
+var numbersConfirm;
+var symbolsConfirm;
+var lowerCaseConfirm;
+var upperCaseConfirm;
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var symbols = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', '"', '|', '<', ',', '>', '.', '?', '/'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
@@ -13,18 +18,68 @@ function generatePassword() {
   // if prompt is cancelled
   if (!message) {
     alert('Must add a number between 8 and 128');
-  // logs the ammount of characters the user selects
+  // logs the numerical value the user selects
     } else if (message < 8 || message > 128) {
       message = parseInt(prompt('Number must be a numerical value between 8 and 128'));
   // prompts for criteria in the password
     } else {
-      numbers = confirm('Do you want numbers in your password?');
-      symbols = confirm('Do you want symbols in your password?');
-      lowerCase = confirm('Do you want lowercase letters in your password?');
-      upperCase = confirm('Do you want uppercase letters in your password?');
+      numbersConfirm = confirm('Do you want numbers in your password?');
+      symbolsConfirm = confirm('Do you want symbols in your password?');
+      lowerCaseConfirm = confirm('Do you want lowercase letters in your password?');
+      upperCaseConfirm = confirm('Do you want uppercase letters in your password?');
+    }
+    
+  // if all criteria is cancelled, alerts messageTwo
+  if (!numbersConfirm && !symbolsConfirm && !lowerCaseConfirm && !upperCaseConfirm) {
+      messageTwo = alert('Must input criteria for password generation');
+    }
+  // accepts all criteria
+    else if (numbersConfirm && symbolsConfirm && lowerCaseConfirm && upperCaseConfirm) {
+      messageTwo = numbers.concat(symbols, lowerCase, upperCase);
+    }
+  // accepts 3 different criteria
+    else if  (numbersConfirm, symbolsConfirm, lowerCaseConfirm) {
+      messageTwo = numbers.concat(symbols, lowerCase);
+    }
+    else if (numbersConfirm, symbolsConfirm, upperCaseConfirm) {
+      messageTwo = numbers.concat(symbols, upperCase);
+    }
+    else if (symbolsConfirm, lowerCaseConfirm, upperCaseConfirm) {
+      messageTwo = symbols.concat(lowerCase, upperCase);
+    }
+  // accepts 2 different criteria
+    else if (numbersConfirm, symbolsConfirm) {
+      messageTwo = numbers.concat(symbols);
+    }
+    else if (numbersConfirm, lowerCaseConfirm) {
+      messageTwo = numbers.concat(lowerCase);
+    }
+    else if (numbersConfirm, upperCaseConfirm) {
+      messageTwo = numbers.concat(upperCase);
+    }
+    else if (symbolsConfirm, lowerCaseConfirm) {
+      messageTwo = symbols.concat(lowerCase);
+    }
+    else if (symbolsConfirm, upperCaseConfirm) {
+      messageTwo = symbols.concat(upperCase);
+    }
+    else if (lowerCaseConfirm, upperCaseConfirm) {
+      messageTwo = lowerCase.concat(upperCase);
+    }
+  // accepts 1 criteria
+    else if (numbersConfirm) {
+      messageTwo = numbers;
+    }
+    else if (symbolsConfirm) {
+      messageTwo = symbols;
+    }
+    else if (lowerCaseConfirm) {
+      messageTwo = lowerCase;
+    }
+    else if (upperCaseConfirm) {
+      messageTwo = upperCase;
     }
   };
-
 
 
 function writePassword() {
